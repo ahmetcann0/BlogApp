58283fd6794c58ec18cd7b095eef887d0d2ef4d2
Ahmet Can Öztürk <ahmetcanozturk53@gmail.com>
2024-09-15 20:49:11 +0300

    Bug fix

---
+++ client/blog-app/src/pages/Login.tsx
@@ -0,0 +1,18 @@
+// src/pages/Login.tsx
+import React from 'react';
+
+function Login(){
+
+    return(
+
+        <div>
+            login
+        </div>
+
+
+
+    );
+}
+
+
+export default Login
