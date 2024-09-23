e9fe0e6ba00baac8b6add588073c315fbcded34b
Ahmet Can Öztürk <ahmetcanozturk53@gmail.com>
2024-09-23 21:54:01 +0300

    Update

--- client/blog-app/src/components/app_layout.tsx
+++ client/blog-app/src/components/app_layout.tsx
@@ -1,17 +1,19 @@
 // src/components/AppLayout.tsx
-import React from 'react';
-import { Outlet } from 'react-router-dom';
-import AppHomeNavbar from './app_home_navbar';
-import AppFooter from './app_footer';
+import React from "react";
+import { Outlet } from "react-router-dom";
+import AppHomeNavbar from "./app_home_navbar";
+import AppFooter from "./app_footer";
 
 const AppLayout: React.FC = () => {
   return (
     <div className="flex flex-col min-h-screen">
       <AppHomeNavbar /> {/* Static Navbar */}
-      <main className="flex-grow"> {/* Main content area */}
+      <main className="flex-grow mt-24">
+        {" "}
+        {/* Main content area */}
         <Outlet /> {/* Dynamic content */}
       </main>
-      <AppFooter /> {/* Static Footer */}
+      <AppFooter></AppFooter>
     </div>
   );
 };
